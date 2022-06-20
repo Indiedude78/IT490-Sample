d5ea440b7c3f8a684dda168ad500bcf42fd566d8
Smit Joshi <saj39@njit.edu>
2022-06-19 20:03:34 -0400

    testing db with rmq

--- registeruser.php
+++ registeruser.php
@@ -1,6 +1,6 @@
 
 <?php
-require_once(__DIR__ . "lib/configrmq.php");
+require_once(__DIR__ . "/lib/configrmq.php");
 require_once(__DIR__ . "/vendor/autoload.php");
 //include(__DIR__ . "/config_rmq.php");
 use PhpAmqpLib\Connection\AMQPStreamConnection;
